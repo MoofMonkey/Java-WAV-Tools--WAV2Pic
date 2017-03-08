50ad636df4b7aad3b92fc76cbe307c1dbfccd95d
Nikita <tyumencev2004@bk.ru>
2017-03-08 18:14:00 +0300

    Simplified getting of factors

--- com/moofMonkey/PixelData.java
+++ com/moofMonkey/PixelData.java
@@ -1,8 +1,5 @@
 package com.moofMonkey;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class PixelData {
 	int width,  height, pixels;
 	
@@ -14,25 +11,19 @@ public class PixelData {
 	}
 	
 	public static PixelData getPixelData(int num) throws Throwable {
-		ArrayList<Integer> factors = getFactors(num);
-		
-		int width = factors.get(factors.size() - 1); // Get last factor for ~1:1
-		int height = num / width;
+		int xy = floorMax(Math.sqrt(num)); // 1:1
 		
 		return new PixelData (
-			width,
-			height
+			xy,
+			xy
 		);
 	}
 	
-	public static ArrayList<Integer> getFactors(int num) {
-		int upperlimit = (int)(Math.sqrt(num));
-		ArrayList<Integer> factors = new ArrayList<Integer>();
-		for(int i = 1; i <= upperlimit; i += 1)
-			if (num % i == 0)
-				factors.add(i);
+	public static int floorMax(double i) {
+		int floor = (int) (i / 1);
+		if(i > floor)
+			floor =+ 1;
 		
-		Collections.sort(factors);
-		return factors;
+		return floor;
 	}
 }
